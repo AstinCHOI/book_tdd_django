fc8964ddd3323655c66332af0c363b9f4dce2f4d
astinchoi <asciineo@gmail.com>
2015-12-26 20:13:39 +0900

    change to 1.9

--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
+from . import views
 
 urlpatterns = [	
     # url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
     #     name='view_list'
     # ),
     #url(r'^lists/(.+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
-    url(r'^new$', 'lists.views.new_list', name='new_list'),
+    url(r'^(\d+)/$', views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+    url(r'^new$', views.new_list, name='new_list'),
 ]
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from lists import views
 
 urlpatterns = [	
-    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^$', views.home_page, name='home'),
     url(r'^lists/', include('lists.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 ]
