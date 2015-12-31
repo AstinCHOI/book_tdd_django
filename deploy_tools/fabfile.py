0dde230291352ce23595c12cec4a90a4aefbca22
astinchoi <asciineo@gmail.com>
2016-01-01 04:27:10 +0900

    modify fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -29,13 +29,13 @@ def _get_latest_source(source_folder):
     run('cd %s && git reset --hard %s' % (source_folder, current_commit))
 
 def _update_settings(source_folder, site_name):
-    settings_path = source_folder + '/superlists/superlists/settings.py'
+    settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
         'ALLOWED_HOSTS = ["%s"]' % (site_name,)
     )
-    secret_key_file = source_folder + '/superlists/superlists/secret_key.py'
+    secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
@@ -51,11 +51,11 @@ def _update_virtualenv(source_folder):
     )
 
 def _update_static_files(source_folder):
-    run('cd %s && ../virtualenv/bin/python3 superlists/manage.py collectstatic --noinput' % \
+    run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput' % \
         (source_folder,)
     )
 
 def _update_database(source_folder):
-    run('cd %s && ../virtualenv/bin/python3 superlists/manage.py migrate --noinput' % \
+    run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput' % \
         (source_folder,)
     )
