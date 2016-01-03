f36f2f02e36f6a5d4a5013122649654e2a7dd91f
astinchoi <asciineo@gmail.com>
2016-01-04 00:21:20 +0900

    Use get_absolute_url on List model to DRY urls in views

--- lists/views.py
+++ lists/views.py
@@ -25,7 +25,8 @@ def view_list(request, list_id):
             item = Item(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect('/lists/%d/' % (list_.id,))
+            # return redirect('/lists/%d/' % (list_.id,))
+            return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
 
@@ -43,7 +44,8 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error" : error})
     # return redirect('/lists/the-only-list-in-the-world/')
-    return redirect('/lists/%d/' % (list_.id,))
+    # return redirect('/lists/%d/' % (list_.id,))
+    return redirect(list_)
 
 
 # def add_item(request, list_id):
