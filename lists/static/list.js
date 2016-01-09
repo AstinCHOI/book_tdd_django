20a505ebf1c5201579ab0a21e105ff72f751265d
astinchoi <asciineo@gmail.com>
2016-01-09 20:15:44 +0900

    Qunit javascript test

---
+++ lists/static/list.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+//   $('input').on('keypress', function() {
+//     $('.has-error').hide()
+//   });
+// })
+jQuery(document).ready(function ($) {
+  $('input').on('keypress', function() {
+    $('.has-error').hide()
+  });	
+})
