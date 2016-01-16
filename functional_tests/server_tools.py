9a6909c8932cb3508e41b2017616f1aad9aba6ea
astinchoi <asciineo@gmail.com>
2016-01-17 02:46:11 +0900

    del secret

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from os import path
 import subprocess
 
@@ -12,7 +11,6 @@ def create_session_on_server(host, email):
             'create_session_on_server:email={}'.format(email),
             '--host={}'.format(host),
             '--hide=everything,status',
-            '--password={}'.format(settings.STAGING_PASSWORD),
         ],
         cwd=THIS_FOLDER
     ).decode().strip()
@@ -21,7 +19,6 @@ def reset_database(host):
     subprocess.check_call(
         ['fab', 'reset_database',
          '--host={}'.format(host),
-         '--password={}'.format(settings.STAGING_PASSWORD),
         ],
         cwd=THIS_FOLDER
     )
