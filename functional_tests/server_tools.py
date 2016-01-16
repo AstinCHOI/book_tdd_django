5ea4d69fdf8e696c6f8663d0cc744e2f0a383eff
astinchoi <asciineo@gmail.com>
2016-01-16 20:55:53 +0900

    add secret and staging pass

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from os import path
 import subprocess
 
@@ -11,14 +12,16 @@ def create_session_on_server(host, email):
             'create_session_on_server:email={}'.format(email),
             '--host={}'.format(host),
             '--hide=everything,status',
+            '--password={}'.format(settings.STAGING_PASSWORD),
         ],
         cwd=THIS_FOLDER
     ).decode().strip()
 
 def reset_database(host):
     subprocess.check_call(
-        ['fab', 'reset_database', 
+        ['fab', 'reset_database',
          '--host={}'.format(host),
+         '--password={}'.format(settings.STAGING_PASSWORD),
         ],
         cwd=THIS_FOLDER
     )
