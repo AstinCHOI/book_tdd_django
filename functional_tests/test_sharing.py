659aef83fee9f9d950db89452619cb76991a05d3
astinchoi <asciineo@gmail.com>
2016-01-24 02:29:54 +0900

    Create Page objects for Home and List pages, use in sharing FT

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from .base import FunctionalTest
+from .home_and_list_pages import HomePage
 
 
 def quit_if_possible(browser):
@@ -10,20 +11,52 @@ def quit_if_possible(browser):
 class SharingTest(FunctionalTest):
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
-        self.create_pre_authenticated_session('astinchoi@mockmyid.com')
-        astin_browser = self.browser
-        self.addCleanup(lambda: quit_if_possible(astin_browser))
+        self.create_pre_authenticated_session('edith@example.com')
+        edith_browser = self.browser
+        self.addCleanup(lambda: quit_if_possible(edith_browser))
 
         oni_browser = webdriver.Firefox()
         self.addCleanup(lambda: quit_if_possible('oniciferous@example.com'))
         self.create_pre_authenticated_session('oniciferous@example.com')
 
-        self.browser = astin_browser
-        self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('Get help\n')
+        self.browser = edith_browser
+        # self.browser.get(self.server_url)
+        # self.get_item_input_box().send_keys('Get help\n')
+        list_page = HomePage(self).start_new_list('Get help')
 
-        share_box = self.browser.find_element_by_css_selector('input[name=email]')
+        # share_box = self.browser.find_element_by_css_selector('input[name=email]')
+        # # self.assertEqual(
+        # #     share_box.get_attribute('placeholder'),
+        # #     'your-friend@example.com'
+        # # )
+        # self.wait_for(
+        #     lambda: self.assertEqual(
+        #         self.browser.find_element_by_css_selector(
+        #             'input[name=email]'
+        #         ).get_attribute('placeholder'),
+        #         'your-friend@example.com'
+        #     )
+        # )
+        share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute('placeholder'),
             'your-friend@example.com'
         )
+
+        list_page.share_list_with('oniciferous@example.com')
+
+        self.browser = oni_browser
+        HomePage(self).go_to_home_page().go_to_my_lists_page()
+
+        self.browser.find_element_by_link_text('Get Help').click()
+
+        self.wait_for(lambda: self.assertEqual(
+            list_page.get_list_owner(),
+            'edith@example.com'
+        ))
+
+        list_page.add_new_item('Hi, Edith!')
+
+        self.browser = edith_browser
+        self.browser.refresh()
+        list_page.wait_for_new_item_in_list('Hi Edith!', 2)
